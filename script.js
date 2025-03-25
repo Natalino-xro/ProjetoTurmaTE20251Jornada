f2956a91590df0501cdb4102afca128634f73027
Natalino-xro <fernandes.natalino@escola.pr.gov.br>
2025-03-25 17:34:45 +0000

    Correção js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-document.addEventListener('DOMContentLoadea', function()){
-    let tamanhoAtualFonte = 1;
+document.addEventListener('DOMContentLoaded', function() {
+    let tamanhoAtualFonte=1;
+    const aumentaFonteBotao=document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao=document.getElementById('diminuir-fonte');
+aumentaFonteBotao.addEventListener('click', function(){;
+        tamanhoAtualFonte +=0.1;
+        document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+    })
+diminuiFonteBotao.addEventListener('click', function(){;
+        tamanhoAtualFonte -=0.1;
+        document.body.style.fontSize=`${tamanhoAtualFonte}rem`
+    })
 
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 
-    aumentaFonteBotao.addEventListener('click', function()){
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize= `$(tamanhoAtualFonte)rem` 
-    }
-}
+})
